65dfb0f92fa2297cdc412a980b287b571d36fb4e
Dmitry757 <glv11@mail.ru>
2022-05-20 01:09:54 +0300

    add methods from task 1-3

--- src/org/dng/Main.java
+++ src/org/dng/Main.java
@@ -1,8 +1,21 @@
 package org.dng;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        int[] arr1 = new int[5];
+
+        arr1[0]=1;
+        arr1[1]=2;
+        arr1[2]=3;
+        arr1[3]=4;
+
+        System.arraycopy(arr1, 0, arr1, 1, 4);
+        System.out.println(Arrays.toString(arr1));
+
     }
 }
--- src/org/dng/MyArrayList.java
+++ src/org/dng/MyArrayList.java
@@ -22,23 +22,23 @@ import java.util.stream.Stream;
  * <p>
  * Задание 1 Создать два конструктора.
  * ■С параметром типа int. Задающего начальную емкость массива. Принимает один параметр (задает capacity),
- *      выделяет память под массив (size = 0).
+ * выделяет память под массив (size = 0).
  * ■По умолчанию (без параметров). Который выделяет память под массив на 10 элементов, равных нулю (capacity = 10, size = 0).
- *      Переиспользовать конструктор с параметрами для уменьшения кода.
+ * Переиспользовать конструктор с параметрами для уменьшения кода.
  * <p>
  * Задание 2
  * Реализовать методы:
  * ■геттеры для size. Сеттера для size не должно быть!
  * ■переопределить метод toString и реализовать строковое представление  элементов массива через пробел.
  * ■ensureCapacity – закрытый метод! проверяет, достаточно ли резерва памяти для хранения указанного в параметре количества элементов.
- *      Если значение параметра меньше текущего capacity, то ничего не происходит.
- *      Если значение параметра больше текущего capacity, то массив пересоздает-ся, памяти выделяется в 1,5 раза + 1 элемент больше.
- *      Существующие элементы переносятся в новый массив.  Существующие элементы не должны быть потеряны.
+ * Если значение параметра меньше текущего capacity, то ничего не происходит.
+ * Если значение параметра больше текущего capacity, то массив пересоздает-ся, памяти выделяется в 1,5 раза + 1 элемент больше.
+ * Существующие элементы переносятся в новый массив.  Существующие элементы не должны быть потеряны.
  * <p>
  * Задание 3
  * Реализовать методы:
  * ■pushBack (добавление элемента в конец массива. Должна быть проверка, достаточно ли памяти!
- *      Если памяти не достаточно увеличить емкость массива данных);
+ * Если памяти не достаточно увеличить емкость массива данных);
  * ■popFront (удаление первого элемента из массива);
  * ■pushFront (добавление нового элемента в начало массива);
  * ■insert (вставка нового элемента в массив по указанному индексу, с проверкой на выход за пределы массива);
@@ -53,9 +53,9 @@ import java.util.stream.Stream;
  * ■isEmpty (метод возвращает true, если size = 0, и false  в  обратном случае);
  * ■trimToSize (метод подгоняет значение capacity под size, естественно с перевыделением памяти);
  * ■indexOf (линейный поиск слева направо первого вхождения в массив указанного значения.
- *      В результате работы вернуть индекс найденного элемента, а eсли ничего не найдено, вернуть -1);
+ * В результате работы вернуть индекс найденного элемента, а eсли ничего не найдено, вернуть -1);
  * ■lastIndexOf (линейный поиск справа налево вхождения в массив указанного значения.
- *      В результате работы вернуть индекс найденного элемента, а eсли ничего не найдено, вернуть -1).
+ * В результате работы вернуть индекс найденного элемента, а eсли ничего не найдено, вернуть -1).
  * <p>
  * Задание 5
  * Реализовать методы:
@@ -65,17 +65,17 @@ import java.util.stream.Stream;
  * Задание 6
  * Реализовать методы:
  * ■equals (в качестве параметра передается ссылка на другой объект класса MyArrayList.
- *      Метод сравнивает массивы не только по количеству элементов, но и по их содержимому);
+ * Метод сравнивает массивы не только по количеству элементов, но и по их содержимому);
  * ■getElementAt (возврат копии элемента массива по указанному индексу, с проверкой на выход за пределы массива);
  * ■переопределить метод clone – метод создает точную копию MyArrayList и возвращает ссылку на эту копию (неглубокое копирование).
  */
-public class MyArrayList <T> {
+public class MyArrayList<T> {
     private final int INIT_SIZE = 10;
     private final int RESIZE_QUANTITY = 2;
     private final int CONDITION4CUT = 2;
     private int pointerOnLastElement = 0; //pointerOnLastElement
     private int capacity = INIT_SIZE; //current size of array
-//    private Object[] dataArray = new Object[INIT_SIZE];
+    //    private Object[] dataArray = new Object[INIT_SIZE];
     private Object[] dataArray;
 
     //**** Constructors ****
@@ -83,6 +83,7 @@ public class MyArrayList <T> {
         dataArray = new Object[INIT_SIZE];
         capacity = dataArray.length;
     }
+
     public MyArrayList(int capacity) {
         dataArray = new Object[capacity];
         capacity = dataArray.length;
@@ -91,14 +92,14 @@ public class MyArrayList <T> {
 
     //*** Getters **********
     public int getSize() {
-        return pointerOnLastElement+1;
+        return pointerOnLastElement + 1;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (Object o:dataArray) {
-            sb.append(o.toString()+" ");
+        for (Object o : dataArray) {
+            sb.append(o.toString() + " ");
         }
         return sb.toString();
     }
@@ -109,12 +110,13 @@ public class MyArrayList <T> {
      * Если значение параметра меньше текущего capacity, то ничего не происходит. Если значение параметра больше текущего capacity,
      * то массив пересоздает-ся, памяти выделяется в 1,5 раза + 1 элемент больше. Существующие элементы переносятся в новый массив.
      * Существующие элементы не должны быть потеряны.
+     *
      * @param num - number of elements which we need to storage in array
      */
-    private void ensureCapacity(int num){
+    private void ensureCapacity(int num) {
         capacity = dataArray.length;
         if (capacity < num) {
-            int newSize = (int)(capacity * 1.5) + 1 ;
+            int newSize = (int) (capacity * 1.5) + 1;
             Object[] newArray = new Object[newSize];
             System.arraycopy(dataArray, 0, newArray, 0, capacity);
             dataArray = newArray;
@@ -124,14 +126,134 @@ public class MyArrayList <T> {
 
     /**
      * add item to end of array
+     *
      * @param item - item for adding
      */
     public void pushBack(T item) {
         ensureCapacity(pointerOnLastElement + 1 + 1);//current size = pointerOnLastElement+1 and need one place for new item
-        dataArray[pointerOnLastElement+1] = item;
+        dataArray[pointerOnLastElement + 1] = item;
         pointerOnLastElement++;
     }
 
+    /**
+     * add item to start of array
+     *
+     * @param item - item for adding
+     */
+    public void pushFront(T item) {
+        insert(item, 0);
+    }
+
+    /**
+     * insert item to array at index idx
+     * return true if success or false if idx is in out of bounds of array
+     *
+     * @param item
+     * @param idx
+     */
+    public boolean insert(T item, int idx) {
+        boolean success = false;
+        ensureCapacity(pointerOnLastElement + 1 + 1);//current size = pointerOnLastElement+1 and need one place for new item
+        if (idx == 0) {
+            System.arraycopy(dataArray, 0, dataArray, 1, pointerOnLastElement + 1);
+            dataArray[0] = item;
+            pointerOnLastElement++;
+            success = true;
+        } else if (idx <= pointerOnLastElement) {
+            System.arraycopy(dataArray, idx + 1, dataArray, idx + 1, pointerOnLastElement + 1 - idx);
+            dataArray[idx] = item;
+            pointerOnLastElement++;
+            success = true;
+        }
+        return success;
+    }
+
+    /**
+     * remove item from start of array
+     *
+     * @param item - item for adding
+     */
+    public boolean popFront(T item) {
+        return removeAt(0);
+    }
+
+    public boolean popBack(){
+        return removeAt(pointerOnLastElement);
+    }
+    /**
+     * remove element at position idx (first position has index 0 )
+     *
+     * @param idx - index of erasing element of array
+     */
+    public boolean removeAt(int idx) {
+        boolean success = false;
+
+        if ( (idx == 0) && (dataArray[0] != null) ) {
+            System.arraycopy(dataArray, 1, dataArray, 0, pointerOnLastElement);
+            dataArray[pointerOnLastElement] = 0;
+            pointerOnLastElement--;
+            success = true;
+        } else if (idx == pointerOnLastElement) {
+            dataArray[idx] = null;
+            pointerOnLastElement--;
+            success = true;
+        } else if ((idx > 0) && (idx <= pointerOnLastElement)) {
+            System.arraycopy(dataArray, idx + 1, dataArray, idx + 0, pointerOnLastElement - idx);
+            pointerOnLastElement--;
+            success = true;
+        }
+
+        if (CONDITION4CUT < (dataArray.length - pointerOnLastElement)) {
+            Object[] newArray = new Object[dataArray.length - 1];
+            newArray = new Object[pointerOnLastElement];
+            System.arraycopy(dataArray, 0, newArray, 0, pointerOnLastElement);
+            dataArray = newArray;
+            newArray = null;//???? does this operator needs for releasing of memory?
+        }
+        return success;
+    }
+
+    /**
+     * remove from array element which is equal to item
+     * and return true if array contains value or false in other case
+     *
+     * @param item
+     */
+    public boolean remove(T item) {
+        boolean success = false;
+        for (int i = 0; i < pointerOnLastElement; i++) {
+            if (dataArray[i].equals(item)) {
+                removeAt(i);
+                success = true;
+                break;
+            }
+        }
+        return success;
+    }
+
+    /**
+     * remove from array all elements which are equal to item
+     * and return true if array contains value or false in other case
+     *
+     * @param item
+     */
+    public boolean removeAll(T item) {
+        boolean success = false;
+        for (int i = 0; i < pointerOnLastElement; i++) {
+            if (dataArray[i].equals(item)) {
+                removeAt(i);
+                success = true;
+            }
+        }
+        return success;
+    }
+
+    public void clear(){
+//        dataArray = new Object[0];
+//        capacity = 0;
+        dataArray = new Object[capacity];
+        pointerOnLastElement = 0;
+    }
 
     public void set(int idx, T item) {
         if ((0 < idx) && (idx <= pointerOnLastElement)) {
@@ -139,23 +261,6 @@ public class MyArrayList <T> {
         }
     }
 
-
-    public void remove(int idx) {
-        Object[] newArray = new Object[dataArray.length - 1];
-        if (idx > 0) {
-            System.arraycopy(dataArray, 0, newArray, 0, idx);
-        }
-        System.arraycopy(dataArray, idx + 1, newArray, idx, pointerOnLastElement - idx - 1);
-        dataArray = newArray;
-        pointerOnLastElement--;
-
-        if (CONDITION4CUT < (dataArray.length - pointerOnLastElement)) {
-            newArray = new Object[pointerOnLastElement];
-            System.arraycopy(dataArray, 0, newArray, 0, pointerOnLastElement);
-            dataArray = newArray;
-        }
-        newArray = null;
-    }
 
     public void show(String oper) {
         System.out.println("***** " + oper + " ****");
